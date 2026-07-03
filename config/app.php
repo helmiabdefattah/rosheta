85c2fca9e6214550c14b5b58f60ddd931797de50
Helmi Abdelfattah <helmi.abdelfattah@gmail.com>
2026-07-04 02:42:55 +0300

    fux

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'Africa/Cairo'),
 
     'locale' => env('APP_LOCALE', 'en'),
 
